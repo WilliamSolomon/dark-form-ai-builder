72e925a11cd224f0273b31d3116a567576982c7c
William Solomon <wsolomon@live.com>
2024-06-05 09:54:28 -0400

    Add Header and Logo

--- app/page.js
+++ app/page.js
@@ -5,14 +5,6 @@ export default function Home() {
   return (
     <main>
      
-      <Button> Hello World</Button>
-      <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#A120DE", // Global primary color
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
