b1a16df34c4dc1d99ffa4b9e03350ab595ab7699
William Solomon <wsolomon@live.com>
2024-06-05 17:54:31 -0400

    Integrate Google Gemini and link to Database

--- app/dashboard/_components/CreateForm.jsx
+++ app/dashboard/_components/CreateForm.jsx
@@ -9,15 +9,39 @@ import {
     DialogTitle,
 } from "@/components/ui/dialog"
 import { Textarea } from "@/components/ui/textarea"
+import { AIChatSession } from '@/configs/aiModel'
+import { useUser } from '@clerk/nextjs'
+import { JsonForms } from '@/configs/schema'
+import { db } from '@/configs/index'
+import moment from 'moment/moment'
 
-
+const testPrompt = `Description: provide a student registration form for a coding workshop on react and react native. On the basis of this description, please provide a form in JSON format format with a form title and form subheading. Include the following fields: 
+form name, placeholder, name, and form label, fieldtype, field required. Again, in json format.`
 
 function CreateForm() {
     const [openDialog, setOpenDialog] = useState(false)
     const [userInput, setUserInput] = useState();
+    const [loading, setLoading] = useState();
+    const {user}=useUser();
 
-    const onCreateForm = () => {
+    const onCreateForm = async () => {
         console.log(userInput)
+        setLoading(true)
+        const result = await AIChatSession.sendMessage("Description:" + userInput + testPrompt);
+        console.log(result.response.text());
+        if (result.response.text()) 
+        {
+            const resp=await db.insert(JsonForms)
+            .values({
+                jsonform:result.response.text(),
+                createdBy:user?.primaryEmailAddress?.emailAddress,
+                createdAt:moment().format('DD/MM/YYYY')
+
+            }).returning({id:JsonForms.id})
+            console.log("New Form ID",resp[0].id)
+            setLoading(false)
+        }
+        setLoading(false)
     }
 
     return (
@@ -28,14 +52,16 @@ function CreateForm() {
                     <DialogHeader>
                         <DialogTitle>Create New Form</DialogTitle>
                         <DialogDescription>
-                            <Textarea className="my-2"
+                            <Textarea className="my-2" disabled={loading}
                                 onChange={(event) => setUserInput(event.target.value)}
                                 placeholder="Write a description of your form" />
                             <div className='flex gap-2 my-3 justify-end'>
                                 <Button variant="destructive"
                                     onClick={() => setOpenDialog(false)}
                                 >Cancel</Button>
-                                <Button onClick={() => onCreateForm()}>Create</Button>
+                                <Button
+                                    disabled={loading}
+                                    onClick={() => onCreateForm()}>Create</Button>
                             </div>
                         </DialogDescription>
                     </DialogHeader>
--- configs/schema.js
+++ configs/schema.js
@@ -3,6 +3,6 @@ import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 export const JsonForms=pgTable('forms', {
     id:serial('id').primaryKey(),
     jsonform:text('jsonform').notNull(),
-    createdBy:varchar('createBy').notNull(),
-    craetedAt:varchar('createdAt').notNull()
+    createdBy:varchar('createdBy').notNull(),
+    createdAt:varchar('createdAt').notNull()
 })
